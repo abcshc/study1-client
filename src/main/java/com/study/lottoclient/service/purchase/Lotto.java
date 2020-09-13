696c3572e7b17f818218710c9a7087b301bd6e93
Heechul So <thrrkthrrk@gmail.com>
2020-09-13 11:40:00 +0900

    2주차 로또 자동번호 생성

--- src/main/java/com/study/lottoclient/service/purchase/Lotto.java
+++ src/main/java/com/study/lottoclient/service/purchase/Lotto.java
@@ -1,10 +1,46 @@
 package com.study.lottoclient.service.purchase;
 
-import lombok.AllArgsConstructor;
-
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-@AllArgsConstructor
 public class Lotto {
     private List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        List<Integer> remains = IntStream.range(1, 46).boxed().collect(Collectors.toList());
+
+        if (this.isInvalidNumber(numbers)) {
+            throw new RuntimeException();
+        }
+        // TODO: 번호 자동생성 6개
+        if (numbers.size() < 6) {
+            for (Integer number : numbers) {
+                remains.removeIf(it -> it.equals(number));
+            }
+
+            // 지우는거 완료
+
+            for (int i = 0; i < 6 - numbers.size(); i++) {
+                Random random = new Random();
+                int value = random.nextInt(45) % remains.size();
+                int next = remains.get(value);
+                remains.remove(value);
+                numbers.add(next);
+            }
+
+        } else if (numbers.size() > 6) {
+            throw new RuntimeException();
+        }
+    }
+
+    private boolean isInvalidNumber(List<Integer> input) {
+        for (Integer number : input) {
+            if (number < 1 || number > 45) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/com/study/lottoclient/service/purchase/PurchaseServiceImpl.java
+++ src/main/java/com/study/lottoclient/service/purchase/PurchaseServiceImpl.java
@@ -9,9 +9,17 @@ public class PurchaseServiceImpl implements PurchaseService {
 
     @Override
     public void purchase(int amount, List<Lotto> lottos) {
-        //TODO : amount 랑 로또의 사이즈랑 비교해서 잘못된 요청인지 확인
-        //       잘못된 로또 번호 확인 (중복된 번호나, 45 넘는 숫자가 있는지 아니면 1보다 작은 숫자가 있는지)
-        //       로또 구매 내역 기록
+        // lotto는 개당 1000원
+        if(amount != lottos.size() * 1000) {
+            // TODO: 잘못된 금액인 경우 예외처리
+            throw new RuntimeException();
+        }
+
+        // amount - 구매 금액
+        //TODO : amount 랑 로또의 사이즈랑 비교해서 잘못된 요청인지 확인 (/)
+        //       잘못된 로또 번호 확인 (중복된 번호나, 45 넘는 숫자가 있는지 아니면 1보다 작은 숫자가 있는지) (/)
         //       자동 생성 필요 확인과 자동 생성
+        //       로또 구매 내역 기록
     }
 }
+
