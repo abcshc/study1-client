2fd32df5e8f28b999ffd128d187b01f1f3fe9d34
Heechul So <thrrkthrrk@gmail.com>
2020-11-01 14:12:25 +0900

    8주차 PurchaseController Unit test 설정

--- src/test/java/com/study/lottoclient/web/PurchaseControllerTest.java
+++ src/test/java/com/study/lottoclient/web/PurchaseControllerTest.java
@@ -2,10 +2,8 @@ package com.study.lottoclient.web;
 
 import com.study.lottoclient.service.purchase.PurchaseService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Optional;
 
@@ -14,12 +12,10 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(PurchaseController.class)
 class PurchaseControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-    @MockBean
     private PurchaseService purchaseService = mock(PurchaseService.class);
+    private PurchaseController purchaseController = new PurchaseController(purchaseService);
+    private MockMvc mockMvc = MockMvcBuilders.standaloneSetup(purchaseController).build();
 
     @Test
     void test_getLottoById_responseNotFound() throws Exception {
